4163182824c6f3edf1027943c2e1198a03042d78
Jacob Koziej <jacobkoziej@gmail.com>
2025-02-15 18:09:30 -0500

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "The Cooper Union - ECE 464: Databases";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC (
+          let
+            pre-commit-bin = "${lib.getBin pkgs.pre-commit}/bin/pre-commit";
+
+          in
+          {
+            packages = with pkgs; [
+              pre-commit
+            ];
+
+            shellHook = ''
+              ${pre-commit-bin} install --allow-missing-config > /dev/null
+            '';
+          }
+        );
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
