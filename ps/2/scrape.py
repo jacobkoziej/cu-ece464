20bf00e53f7009334498171d96d585fc8bfe7c19
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-24 02:45:13 -0400

    Add `scrape.py`

---
+++ ps/2/scrape.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# scape.py -- scraper for <https://quotes.toscrape.com/>
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(main())
