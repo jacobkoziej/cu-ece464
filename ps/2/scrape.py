bbe636e5a5ac0add8641330c37fc477307006d79
Jacob Koziej <jacobkoziej@gmail.com>
2025-03-24 03:02:37 -0400

    Iterate over pages

--- ps/2/scrape.py
+++ ps/2/scrape.py
@@ -4,12 +4,44 @@
 # scape.py -- scraper for <https://quotes.toscrape.com/>
 # Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
 
+import argparse
+import sys
+
+import requests
+
+from http import HTTPStatus
+from typing import Final
+from itertools import count
+
+
+_BASE_URL: Final[str] = "https://quotes.toscrape.com"
+
 
 def main() -> None:
-    pass
+    argparser = argparse.ArgumentParser(
+        description="scraper for <https://quotes.toscrape.com/>",
+    )
+
+    argparser.add_argument(
+        "-p",
+        "--pages",
+        help="pages to scrape",
+        metavar="N",
+        type=int,
+    )
+
+    args = argparser.parse_args()
+
+    pages = count(1) if args.pages is None else range(1, args.pages + 1)
+
+    for i in pages:
+        url = _BASE_URL + f"/page/{i}/"
+
+        request = requests.get(url)
+
+        if request.status_code != HTTPStatus.OK:
+            sys.exit(f"<{url}> status: {request.status_code}")
 
 
 if __name__ == "__main__":
-    import sys
-
     sys.exit(main())
