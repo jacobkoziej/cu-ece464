ce355ec5f74186215626aea5f54f6046ae53cfff
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-26 22:11:11 -0400

    Define `Authenticate`

---
+++ jankins/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# __init__.py -- jankins
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+from . import (
+    client,
+    message,
+    server,
+)
+
+__all__ = [
+    "client",
+    "message",
+    "server",
+]
---
+++ jankins/message.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# message.py -- message definitions
+# Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
+
+from pydantic import BaseModel
+
+
+class Authenticate(BaseModel):
+    username: str
+    password: str
