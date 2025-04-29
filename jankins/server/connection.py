fcad6558a14f7b5038dafa64d087e98fe5468e40
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-29 16:01:38 -0400

    Handle client authentication

--- jankins/server/connection.py
+++ jankins/server/connection.py
@@ -3,6 +3,7 @@
 # connection.py -- connection handler
 # Copyright (C) 2025  Jacob Koziej <jacobkoziej@gmail.com>
 
+from collections import deque
 from socketserver import (
     BaseRequestHandler,
     TCPServer,
@@ -13,14 +14,19 @@ from typing import Optional
 from loguru import logger
 from msgpack import Unpacker
 
+from ..message import (
+    Authenticate,
+)
 from ..serial import decode
 from .config import Config
+from .db import Database
 
 
 class Handler(BaseRequestHandler):
     config: Optional[Config]
 
-    def setup(self) -> None: ...
+    def setup(self) -> None:
+        self.db = Database(self.config.database_path)
 
     def handle(self) -> None:
         sock = self.request
@@ -29,6 +35,8 @@ class Handler(BaseRequestHandler):
         logger.info(f"got connection: {sock.getpeername()}")
 
         unpacker = Unpacker(object_hook=decode)
+
+        msgs = deque()
 
         while True:
             buf = sock.recv(config.recieve_bufsize)
@@ -39,9 +47,26 @@ class Handler(BaseRequestHandler):
             unpacker.feed(buf)
 
             for msg in unpacker:
-                logger.info(msg)
+                msgs.append(msg)
 
-    def finish(self) -> None: ...
+        if not msgs:
+            logger.warn("got no messages")
+            return
+
+        auth = msgs.popleft()
+
+        if not isinstance(auth, Authenticate) or not (
+            uid := self.db.authenticate(auth)
+        ):
+            logger.warning(f"authentication failed for user: {auth.username}")
+            return
+
+        logger.success(f"authenticated user: {auth.username}")
+
+        _ = uid
+
+    def finish(self) -> None:
+        del self.db
 
 
 class Server(ThreadingMixIn, TCPServer):
