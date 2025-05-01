cefd526a5c5ce01115af6aa65ab442dcd9042cd5
Jacob Koziej <jacobkoziej@gmail.com>
2025-05-01 01:15:30 -0400

    Add ability for clients to add actions

--- jankins/client/__main__.py
+++ jankins/client/__main__.py
@@ -8,19 +8,80 @@ import sys
 
 import yaml
 
-from argparse import ArgumentParser
+from argparse import (
+    ArgumentParser,
+    Namespace,
+)
+from importlib import import_module
 from pathlib import Path
 
 from loguru import logger
-from msgpack import Packer
 from platformdirs import user_config_dir
 
-from ..message import Authenticate
-from ..serial import encode
+from ..message import (
+    Action,
+    Authenticate,
+    Success,
+)
+from ..serial import (
+    rx,
+    tx,
+)
 from .config import Config
 
 
-def main() -> None:
+def _action(args: Namespace, config: Config) -> int:
+    sock = _config2sock(config)
+
+    logger.debug(f"connecting to {sock.getpeername()}")
+
+    auth = _config2auth(config)
+
+    logger.debug(f"authenticating as: {auth.username}")
+
+    action = _args2action(args)
+
+    tx(sock, [auth, action])
+    sock.shutdown(socket.SHUT_WR)
+
+    msgs = rx(sock)
+
+    if not msgs:
+        logger.error("got no response from server")
+        sock.close()
+        return 1
+
+    success = msgs.popleft()
+
+    if not isinstance(success, Success):
+        logger.error("got incorrect respones")
+        sock.close()
+        return 1
+
+    if not success.success:
+        logger.error("failed to add action")
+
+    else:
+        logger.success("added action")
+
+    sock.close()
+
+    return int(not success.success)
+
+
+def _args2action(args: Namespace) -> Action:
+    return Action(name=args.name, command=args.command)
+
+
+def _config2sock(config: Config) -> socket.socket:
+    return socket.create_connection((config.hostname, config.port))
+
+
+def _config2auth(config: Config) -> Authenticate:
+    return Authenticate(username=config.username, passwd=config.passwd)
+
+
+def main() -> int:
     parser = ArgumentParser(
         description="jankins client",
     )
@@ -37,6 +98,27 @@ def main() -> None:
         "--verbose",
         action="store_true",
         help="enable verbose logging",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="sub_command",
+        required=True,
+    )
+
+    action_parser = subparsers.add_parser("action")
+    action_parser.add_argument(
+        "-c",
+        "--command",
+        help="action command",
+        required=True,
+        type=str,
+    )
+    action_parser.add_argument(
+        "-n",
+        "--name",
+        help="action name",
+        required=True,
+        type=str,
     )
 
     args = parser.parse_args()
@@ -57,24 +139,10 @@ def main() -> None:
 
     config = Config.model_validate(config)
 
-    address = (config.hostname, config.port)
+    module = import_module(__name__)
+    sub_command = getattr(module, f"_{args.sub_command}")
 
-    logger.debug(f"connecting to {address}")
-
-    sock = socket.create_connection(address)
-
-    packer = Packer(default=encode)
-
-    auth = Authenticate(
-        username=config.username,
-        passwd=config.passwd,
-    )
-
-    logger.debug(f"authenticating as: {auth.username}")
-
-    sock.sendall(packer.pack(auth))
-
-    sock.close()
+    return sub_command(args, config)
 
 
 if __name__ == "__main__":
--- jankins/server/connection.py
+++ jankins/server/connection.py
@@ -13,15 +13,35 @@ from typing import Optional
 from loguru import logger
 
 from ..message import (
+    Action,
     Authenticate,
+    Success,
 )
-from ..serial import rx
+from ..serial import (
+    rx,
+    tx,
+)
 from .config import Config
 from .db import Database
 
 
 class Handler(BaseRequestHandler):
     config: Optional[Config]
+
+    def _handle_Action(self, uid: int, action: Action) -> bool:
+        try:
+            self.db.add_action(action)
+
+        except Exception:
+            logger.error(f"failed to add action: {action.name}")
+            return False
+
+        logger.success(f"added action: {action.name}")
+
+        return True
+
+    def _return_success(self, success: bool) -> None:
+        tx(self.request, [Success(success=success)])
 
     def setup(self) -> None:
         self.db = Database(self.config.database_path)
@@ -36,6 +56,7 @@ class Handler(BaseRequestHandler):
 
         if not msgs:
             logger.warning("got no messages")
+            self._return_success(False)
             return
 
         auth = msgs.popleft()
@@ -44,11 +65,26 @@ class Handler(BaseRequestHandler):
             uid := self.db.authenticate(auth)
         ):
             logger.warning(f"authentication failed for user: {auth.username}")
+            self._return_success(False)
             return
 
         logger.success(f"authenticated user: {auth.username}")
 
-        _ = uid
+        sub_command = msgs.popleft()
+        sub_command_handler = getattr(
+            self,
+            f"_handle_{type(sub_command).__name__}",
+            None,
+        )
+
+        if sub_command_handler is None:
+            logger.error("unknown sub-command encountered")
+            self._return_success(False)
+            return
+
+        success = sub_command_handler(uid, sub_command)
+
+        self._return_success(success)
 
     def finish(self) -> None:
         del self.db
